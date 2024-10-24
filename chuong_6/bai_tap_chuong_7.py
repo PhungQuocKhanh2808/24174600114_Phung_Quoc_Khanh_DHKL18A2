c395a07424f4599ffe6a22382fd217b9d75a41b0
Phung Quoc Khanh <pqkhanh.24174600114@sv.uneti.edu.vn>
2024-10-24 09:38:52 +0700

    bai_2.py

--- chuong_6/bai_tap_chuong_7.py
+++ chuong_6/bai_tap_chuong_7.py
@@ -29,7 +29,7 @@ print(f"Thể tích của khối trụ: {the_tich}")
 # bai 2 
 #tu so = -x + (x**2 + 4)**(1/2)
 #mau so = (x**4 + 1)**(1/7)
-#f_x = (-x + (x**2 + 4)**(1/2)/(x**4 + 1)**(1/7))
+f_x = (-x + (x**2 + 4)**(1/2)/(x**4 + 1)**(1/7))
 x = float(input("nhap gia tri cua x: "))
 f_x = (-x + (x**2 + 4)**(1/2)/(x**4 + 1)**(1/7))
 print(f"Gia tri cua f(x) = {f_x:.2f}")
